6a55168cdaaf1959036747e39d65cd6f67f66230
Cedric555 <59996636+Cedric555@users.noreply.github.com>
2020-02-11 10:00:28 +0100

    récupération de données

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -19,35 +19,13 @@ class Post
     private $id;
 
     /**
-<<<<<<< HEAD
-     * @ORM\Column(type="text")
-=======
      * @ORM\Column(type="string", length=350, nullable=true)
->>>>>>> 4ecfc66a35ab89cac7c15c730e58818aadbb1659
      */
     private $contenu;
 
     /**
      * @ORM\Column(type="datetime")
      */
-<<<<<<< HEAD
-    private $dateTime;
-
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\ImagePost", mappedBy="post")
-     */
-    private $imagePosts;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="posts")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $User;
-
-    public function __construct()
-    {
-        $this->imagePosts = new ArrayCollection();
-=======
     private $datepost;
 
     /**
@@ -64,7 +42,6 @@ class Post
     public function __construct()
     {
         $this->image = new ArrayCollection();
->>>>>>> 4ecfc66a35ab89cac7c15c730e58818aadbb1659
     }
 
     public function getId(): ?int
@@ -77,27 +54,13 @@ class Post
         return $this->contenu;
     }
 
-<<<<<<< HEAD
-    public function setContenu(string $contenu): self
-=======
     public function setContenu(?string $contenu): self
->>>>>>> 4ecfc66a35ab89cac7c15c730e58818aadbb1659
     {
         $this->contenu = $contenu;
 
         return $this;
     }
 
-<<<<<<< HEAD
-    public function getDateTime(): ?\DateTimeInterface
-    {
-        return $this->dateTime;
-    }
-
-    public function setDateTime(\DateTimeInterface $dateTime): self
-    {
-        $this->dateTime = $dateTime;
-=======
     public function getDatepost(): ?\DateTimeInterface
     {
         return $this->datepost;
@@ -118,7 +81,6 @@ class Post
     public function setUser(?User $user): self
     {
         $this->user = $user;
->>>>>>> 4ecfc66a35ab89cac7c15c730e58818aadbb1659
 
         return $this;
     }
@@ -126,18 +88,6 @@ class Post
     /**
      * @return Collection|ImagePost[]
      */
-<<<<<<< HEAD
-    public function getImagePosts(): Collection
-    {
-        return $this->imagePosts;
-    }
-
-    public function addImagePost(ImagePost $imagePost): self
-    {
-        if (!$this->imagePosts->contains($imagePost)) {
-            $this->imagePosts[] = $imagePost;
-            $imagePost->setPost($this);
-=======
     public function getImage(): Collection
     {
         return $this->image;
@@ -148,21 +98,11 @@ class Post
         if (!$this->image->contains($image)) {
             $this->image[] = $image;
             $image->setPost($this);
->>>>>>> 4ecfc66a35ab89cac7c15c730e58818aadbb1659
         }
 
         return $this;
     }
 
-<<<<<<< HEAD
-    public function removeImagePost(ImagePost $imagePost): self
-    {
-        if ($this->imagePosts->contains($imagePost)) {
-            $this->imagePosts->removeElement($imagePost);
-            // set the owning side to null (unless already changed)
-            if ($imagePost->getPost() === $this) {
-                $imagePost->setPost(null);
-=======
     public function removeImage(ImagePost $image): self
     {
         if ($this->image->contains($image)) {
@@ -170,25 +110,9 @@ class Post
             // set the owning side to null (unless already changed)
             if ($image->getPost() === $this) {
                 $image->setPost(null);
->>>>>>> 4ecfc66a35ab89cac7c15c730e58818aadbb1659
             }
         }
 
         return $this;
     }
-<<<<<<< HEAD
-
-    public function getUser(): ?User
-    {
-        return $this->User;
-    }
-
-    public function setUser(?User $User): self
-    {
-        $this->User = $User;
-
-        return $this;
-    }
-=======
->>>>>>> 4ecfc66a35ab89cac7c15c730e58818aadbb1659
 }
