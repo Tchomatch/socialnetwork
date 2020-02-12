f6e3638b176db9779df27a3f4b0558bb865b3516
souhibhdj <59996584+souhibhdj@users.noreply.github.com>
2020-02-12 17:17:15 +0100

    souhib

--- public/js/search.js
+++ public/js/search.js
@@ -1,15 +1,25 @@
 function searchDyn(){
+    // je vérifie que l'utilisateur a bien appuyé sur une touche dans la barre de recherche grace a l'id de l'input
     $( "#search" ).keyup(function() {
+        
         $.ajax({
             method: "POST",
+            // url de la page que je récupérer et inclure dans le code
             url: "/search2",
             // je récupère la valeur de mon input avec son name 
             data: { search: $(this).val() }
         })
         .done(function( dataSearch ) {
+            //je récupère le code et les données dans l'url renseigné au dessus et l'inclure dans la balise contenant l'id searchDyn
             $('#searchDyn').html(dataSearch);
         });
+        
+    });
+  
+    // a chaque fois que je clique en dehors de l'input mes resultat de recherche ne s'affiche plus
+    $( "#search" ).focusout(function() {
+        $("#focus").css("display", "none") 
     });
 
- 
+
 }
